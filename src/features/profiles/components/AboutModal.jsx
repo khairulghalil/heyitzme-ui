e402504d43ebe526a097142d2b6bde92d81359c6
Khairul Ghalil <khairulghalil@Khairul-MacBook.local>
2026-08-20 18:52:11 +0800

    Change ui

--- src/features/profiles/components/AboutModal.jsx
+++ src/features/profiles/components/AboutModal.jsx
@@ -3,36 +3,45 @@ import Modal from "react-bootstrap/Modal";
 function AboutModal({ profileData }) {
   return (
     <Modal.Body className="about-body text-start pt-4">
-      <p>
-        <i className="bi bi-person me-2"></i>
-        Username
-      </p>
-      <span>{profileData.username}</span>
+      <div>
+        <p>
+          <i className="bi bi-person me-2"></i>
+          Username
+        </p>
+        <span>{profileData.username}</span>
+      </div>
 
-      <p>
-        <i className="bi bi-envelope me-2"></i>
-        Registered email
-      </p>
-      <span>{profileData.contact.email}</span>
+      <div>
+        <p>
+          <i className="bi bi-envelope me-2"></i>
+          Registered email
+        </p>
+        <span>{profileData.contact.email}</span>
+      </div>
 
-      <p>
-        <i className="bi bi-link-45deg me-2"></i>Profile URL
-      </p>
-      <span>{`https://heyitzme.com/${profileData.username}`}</span>
+      <div>
+        <p>
+          <i className="bi bi-link-45deg me-2"></i>Profile URL
+        </p>
+        <span>{`https://heyitzme.com/${profileData.username}`}</span>
+      </div>
 
-      <p>
-        <i className="bi bi-info-circle me-2"></i>
-        Status
-      </p>
-      <span>{profileData.status.active ? "Active" : "Inactive"}</span>
+      <div>
+        <p>
+          <i className="bi bi-info-circle me-2"></i>
+          Status
+        </p>
+        <span>{profileData.status.active ? "Active" : "Inactive"}</span>
+      </div>
 
-      <p>
-        <i className="bi bi-calendar-check me-2"></i>
-        Valid until
-      </p>
-      <span>{profileData.status.expiryDate}</span>
-
-      <div className="text-center">
+      <div>
+        <p>
+          <i className="bi bi-calendar-check me-2"></i>
+          Valid until
+        </p>
+        <span>{profileData.status.expiryDate}</span>
+      </div>
+      <div className="footer text-center pt-1 m-0">
         <button type="button" className="btn btn-primary my-3">
           Renew Subscription
         </button>
