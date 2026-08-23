e868997ab7e9bdcdf6a1dd2b80198e9632bca2e4
Khairul Ghalil <khairulghalil@Khairul-MacBook.local>
2026-08-23 13:21:44 +0800

    Add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./features/home";
 import Profiles from "./features/profiles";
 import GenerateCard from "./features/generate";
+import { ScrollToTop } from "./components";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/:username" element={<Profiles />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/edit" element={<Navigate to="/" replace />} />
-      <Route path="/edit/:username" element={<GenerateCard type="edit" />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/:username" element={<Profiles />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/edit" element={<Navigate to="/" replace />} />
+        <Route path="/edit/:username" element={<GenerateCard type="edit" />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,14 @@ import { ToastProvider, useToast } from "./toast";
 import { LoginProvider, useLogin } from "./login";
 import Footer from "./footer";
 import Loader from "./loader";
+import ScrollToTop from "./scrollToTop";
 
-export { ToastProvider, useToast, LoginProvider, useLogin, Footer, Loader };
+export {
+  ToastProvider,
+  useToast,
+  LoginProvider,
+  useLogin,
+  Footer,
+  Loader,
+  ScrollToTop,
+};
